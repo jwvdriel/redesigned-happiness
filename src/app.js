92cb8aa7e14c7f2dd1d981fdef87307880ef6077
jwvdriel <janwillemvandriel@gmail.com>
2025-07-27 14:04:56 +0200

    update javascript code

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const myLibrary = [
   {
     auteur: 'Tad Williams',
     titel: 'Draken Troon',
-    'aantalPagina"s': 860,
+    totaalPaginas: 860,
     id: crypto.randomUUID(),
   },
   {
     auteur: 'Sarah Lark',
     titel: 'Een moedig besluit',
-    "aantal pagina's": 457,
+    totaalPaginas: 457,
     id: crypto.randomUUID(),
   },
 ];
@@ -26,32 +26,34 @@ showFormButton.addEventListener('click', () => {
   }
 });
 
-voegBoekToe.addEventListener('click', () => {
+// hides the form after adding a book to the array
+voegBoekToe.addEventListener('click', (event) => {
+  event.preventDefault();
   if ((formElement.style.display = 'block')) {
     formElement.style.display = 'none';
   }
 });
 
+// the constructor...
 class Book {
   constructor() {
-    // the constructor...
     this.titel = document.querySelector('#title').value;
     this.auteur = document.querySelector('#auteur').value;
-    this.totaalPaginas = document.querySelector('#pagina').value;
+    this.totaalPaginas = parseInt(document.querySelector('#pagina').value);
     this.id = crypto.randomUUID();
   }
 }
 
-function addBookToLibrary(titel, auteur, pagina, id) {
-  // take params, create a book then store it in the array
-  myLibrary.push(new Book(titel, auteur, pagina, id));
+// take params, from the class Book and creates a book then stores it in the array
+function addBookToLibrary(titel, auteur, totaalPaginas, id) {
+  myLibrary.push(new Book(titel, auteur, totaalPaginas, id));
   loopArray();
 }
 
 // loops over the array of added books
 function loopArray() {
   myLibrary.forEach((book) => {
-    console.log(book);
+    console.log(book.titel, book.auteur, book.totaalPaginas, book.id);
   });
 }
 
